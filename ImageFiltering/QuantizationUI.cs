3812d7b69bf967041edeae19caabe8f6f63fc300
shilangyu <xmarcinmarcin@gmail.com>
2022-03-26 11:05:30 +0100

    Add uniform color quantization

---
+++ ImageFiltering/QuantizationUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ImageFiltering
+{
+	public partial class MainWindow : Window
+	{
+		private void Button_Click_21(object sender, RoutedEventArgs e)
+		{
+			Pixels = Pixels?.UniformQuantization(((int)RedSubdivisionsSlider.Value, (int)GreenSubdivisionsSlider.Value, (int)BlueSubdivisionsSlider.Value));
+		}
+	}
+}
