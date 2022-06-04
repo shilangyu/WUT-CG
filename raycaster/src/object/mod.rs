dfa0a1552bfaae4be1778682b04c3904aaf719da
shilangyu <xmarcinmarcin@gmail.com>
2022-06-04 22:23:30 +0200

    Add scene objects

---
+++ raycaster/src/object/mod.rs
@@ -0,0 +1,15 @@
+pub mod sphere;
+
+use crate::{
+    material::PhongMaterial,
+    ray::{Ray, RayIntersection},
+};
+
+pub trait Object {
+    #[must_use]
+    fn material(&self) -> &PhongMaterial;
+
+    /// Returns [None] if does not intersect
+    #[must_use]
+    fn intersect(&self, ray: &Ray) -> Option<RayIntersection>;
+}
---
+++ raycaster/src/object/sphere.rs
@@ -0,0 +1,75 @@
+use macroquad::prelude::*;
+
+use crate::{
+    material::PhongMaterial,
+    ray::{Ray, RayIntersection},
+    utils::*,
+};
+
+use super::Object;
+
+pub struct Sphere {
+    pos: Vec4,
+    radius: f32,
+    material: PhongMaterial,
+}
+
+impl Sphere {
+    pub fn new(pos: Vec3, radius: f32, material: PhongMaterial) -> Self {
+        Self {
+            pos: vec4(pos.x, pos.y, pos.z, 1.),
+            radius,
+            material,
+        }
+    }
+
+    pub fn pos(&self) -> Vec3 {
+        into_vec3(self.pos)
+    }
+}
+
+impl Object for Sphere {
+    fn material(&self) -> &PhongMaterial {
+        &self.material
+    }
+
+    fn intersect(&self, ray: &Ray) -> Option<RayIntersection> {
+        let ps = self.pos();
+
+        // solution to quadriatic equation of time of intersection between the ray and the surface of a sphere
+        let b = 2. * ray.v().dot(ray.p() - ps);
+        let c = (ray.p() - ps).length_squared() - (self.radius * self.radius);
+
+        let off = f32::sqrt(b * b - 4. * c);
+
+        // imaginary solution, not intersecting
+        if off.is_nan() {
+            return None;
+        }
+        // ray tangent to the surface, not intersecting
+        if off == 0. {
+            return None;
+        }
+
+        let t1 = (-b - off) / 2.;
+        let t2 = (-b + off) / 2.;
+
+        // sphere is behind us or we are in the sphere
+        if t1 < 0. || t2 < 0. {
+            return None;
+        }
+
+        // get first ray intersection
+        let t_first = f32::min(t1, t2);
+
+        let intersection_point = ray.p() + t_first * ray.v();
+
+        let unit_normal = (intersection_point - ps).normalize();
+
+        Some(RayIntersection {
+            t: t_first,
+            p: intersection_point,
+            n: unit_normal,
+        })
+    }
+}
